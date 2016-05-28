b1f33bd91562ae25312780e9a2b23fea46aa2bd1
oosuka <aqua_villeneuve@yahoo.co.jp>
2016-05-28 22:53:53 +0900

    Herokuに対応

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
   listeningPort: 3000,
   api: 'untappd',
   breweryDb: {
-    key: '',
+    key: process.env.UJ_BREWERYDB_KEY,
     urlPrefix: 'https://api.brewerydb.com/v2/',
     beerLinkPrefix: 'http://www.brewerydb.com/beer/'
   },
   untappd: {
-    clientId: '',
-    clientSecret: '',
+    clientId: process.env.UJ_UNTAPPD_ID,
+    clientSecret: process.env.UJ_UNTAPPD_SECRET,
     urlPrefix: 'https://api.untappd.com/v4/',
     beerLinkPrefix: 'https://untappd.com/beer/'
   },
-  slackToken: '',
+  slackToken: process.env.UJ_SLACK_TOKEN,
   slackPostMessage: 'https://slack.com/api/chat.postMessage',
   searchLimit: 5
 }
